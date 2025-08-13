f398913cde10f2fefd83ee6324ca6532d7502b22
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-13 20:18:42 +0900

    add props to the header component; add image and button to the Header component; remove duplicates from the index.ts file;

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
 import Header from "@/components/Header";
 import React from "react";
 
-const Page = ({ subHeader, title, userImg }: SharedHeaderProps) => {
+const Page = () => {
   return (
     <main className="wrapper page">
-      <Header />
+      <Header title="All Videos" subHeader="Public Library" />
       <h1 className="text-2xl font-karla">Welcome to ScreenCastly</h1>
     </main>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,52 @@
+import { ICONS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ subHeader, title, userImg }: SharedHeaderProps) => {
+  return (
+    <header className="header">
+      <section className="header-container">
+        <div className="details">
+          {userImg && (
+            <Image
+              src={userImg || "/assets/images/dummy.jpg"}
+              alt="user"
+              width={66}
+              height={66}
+              className="rounded-full aspect-square"
+            />
+          )}
+
+          <article>
+            <p>{subHeader}</p>
+            <h1>{title}</h1>
+          </article>
+        </div>
+
+        <aside>
+          <Link href={"/upload"}>
+            <Image
+              src={"/assets/icons/upload.svg"}
+              alt="upload"
+              width={16}
+              height={16}
+            />
+
+            <span>Upload a video</span>
+          </Link>
+
+          <div className="record">
+            <button className="primary-btn">
+              <Image src={ICONS.record} alt="record" width={16} height={16} />
+
+              <span>Record a video</span>
+            </button>
+          </div>
+        </aside>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
