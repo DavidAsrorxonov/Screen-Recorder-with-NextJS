74299b022c9f7848c501c4121228aec85dbfdcb4
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-13 19:48:56 +0900

    create and fill in index.d.ts for the data types; create Header.tsx in components folder to make it a reusable component;

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
+import Header from "@/components/Header";
 import React from "react";
 
-const Page = () => {
+const Page = ({ subHeader, title, userImg }: SharedHeaderProps) => {
   return (
-    <div>
+    <main className="wrapper page">
+      <Header />
       <h1 className="text-2xl font-karla">Welcome to ScreenCastly</h1>
-    </div>
+    </main>
   );
 };
 
